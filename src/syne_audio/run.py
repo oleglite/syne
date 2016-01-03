f871e51bba2aff6a051ab55348dc625acc7ac336
Oleg Beloglazov <beloglazov.oleg@gmail.com>
2016-01-03 23:00:01 +0300

    add save/load unit

--- src/syne_audio/run.py
+++ src/syne_audio/run.py
@@ -3,16 +3,15 @@
 import pickle
 
 from syne import TimeUnit, make_conf
-from syne.tools import from_plain_signal, object_to_dict
-from syne_audio import conf
+from syne.tools import from_plain_signal, object_to_dict, dict_to_object
+from syne_audio import conf as audio_conf
 
 
-full_conf = make_conf(conf)
-unit = TimeUnit(full_conf)
+FILENAME = '../output'
 
 
-def run():
-    with open('output', 'rb') as f:
+def teach_unit(unit):
+    with open(FILENAME, 'rb') as f:
         data = pickle.load(f)
 
     frames = data['audio']['frames']
@@ -22,13 +21,33 @@ def run():
         if i % 100 == 0:
             print('%d:\t %s%%' % (i, (i * 100 / len(frames))))
 
+        if i > 10000:
+            return
+
+
+def save_unit(unit, path):
     data = {
-        'conf': object_to_dict(full_conf),
+        'conf': object_to_dict(unit.conf),
         'unit': unit.get_data()
     }
 
-    with open('run_unit', 'wb') as f:
+    with open(path, 'wb') as f:
         pickle.dump(data, f)
+
+
+def load_unit(path):
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+
+    conf = make_conf(dict_to_object(data['conf']))
+    unit = TimeUnit(conf, data=data['unit'])
+    return unit
+
+
+def run():
+    conf = make_conf(audio_conf)
+    unit = TimeUnit(conf)
+    teach_unit(unit)
 
 
 if __name__ == '__main__':
